b3ba9495b562d7718cce03106cc85069ace73c12
JoE11-y <Josephedoh77@gmail.com>
2025-03-21 18:07:55 +0100

    chore: web framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use axum::{routing::get, Router};
 mod config;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Welcome to ZeroXbridge Sequencer"
 }
