4c197746e29d07e4d73e3353288f718e75dfb777
guha-rahul <69rahul16@gmail.com>
2025-06-28 14:24:33 +0530

    fix

--- src/events/l1_event_watcher.rs
+++ src/events/l1_event_watcher.rs
@@ -42,7 +42,7 @@ pub async fn fetch_l1_deposit_events(
     let mut conn = db_pool.acquire().await?;
 
     // Load last processed block if available
-    let from_block = match get_last_processed_block(&mut db_pool, BLOCK_TRACKER_KEY).await {
+    let from_block = match get_last_processed_block(&mut conn, BLOCK_TRACKER_KEY).await {
         Ok(Some(last_block)) => last_block + 1,
         Ok(None) => from_block,
         Err(e) => {
